72bc52ae032a101209ef4bf095cb94ae6866eba7
Louis Hoang <bawsing@gmail.com>
2017-05-18 14:45:19 -0700

    Only show done button if todo item is not completed

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -3,12 +3,15 @@ import React, { Component } from 'react'
 class TodoItem extends Component {
 	render(){
 		const { id, name, status, moveToComplete } = this.props.todo
-
+	
 		return(
 			<li>
 				<div>
 					id: { id }, name: { name }, status: { status }
-					<button onClick={() => { this.props.moveToComplete(id) }}>Done</button>
+
+					{ status === 'Active' &&
+					 	<button onClick={() => { this.props.moveToComplete(id) }}>Done</button>
+					}
 				</div>
 			</li>
 		)
